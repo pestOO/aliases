64f9a5e6f1212c85fad855982770dcda4a306a56
Elisey Zamakhov <EliseyZamahov@gmail.com>
2019-04-05 14:17:20 +0200

    Add git setup script

---
+++ SetUpGit.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+git config user.email "EliseyZamahov@gmail.com"
+git config --global user.name "Elisey Zamakhov"
+git config --global user.email Elisey.Zamakhov@tomtom.com
+git config --global help.autocorrect true
+git config --global color.ui true
+git config --global merge.tool vimdiff
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.stash-unapply '!git stash show -p | git apply -R'
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --list
